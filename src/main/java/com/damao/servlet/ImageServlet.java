f76c4dca75bcefe82188811d84a3ecaf77c972f2
YIH <1356605183@qq.com>
2023-11-12 19:10:31 +0800

    add

---
+++ src/main/java/com/damao/servlet/ImageServlet.java
@@ -0,0 +1,24 @@
+package com.damao.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class ImageServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setHeader("refresh", "5");
+        BufferedImage bufferedImage = new BufferedImage(80,20,BufferedImage.TYPE_INT_RGB);
+        Graphics graphics = (Graphics2D) bufferedImage.getGraphics();
+        graphics.setColor(Color.white);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
