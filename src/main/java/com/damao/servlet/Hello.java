af71eff1bd35fd1856ab7cf2971aec04f561f564
YIH <1356605183@qq.com>
2023-11-13 17:49:01 +0800

    add

--- src/main/java/com/damao/servlet/Hello.java
+++ src/main/java/com/damao/servlet/Hello.java
@@ -2,6 +2,7 @@ package com.damao.servlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +15,9 @@ public class Hello extends HttpServlet {
         ServletContext servletContext = this.getServletContext();
         String username = "Damao";
         servletContext.setAttribute("username",username); //save data in context
+        resp.getWriter().print("114514");
+        Cookie[] cookies = req.getCookies();
+
         //super.doGet(req,resp);
     }
 
---
+++ src/main/java/com/damao/servlet/Session.java
@@ -0,0 +1,33 @@
+package com.damao.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class Session extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=utf-8");
+
+        HttpSession session = req.getSession();
+        session.setAttribute("name","damao");
+
+        String id = session.getId();
+        if (session.isNew()){
+            resp.getWriter().write("114514 session id:" + id);
+        }
+        else {
+            resp.getWriter().write("session existed id:" + id);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
